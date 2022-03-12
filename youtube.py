1ddf4d154bd5725c3566aa3d027c06d19821366e
iwanhk <iwan@isotop.top>
2022-03-12 15:52:40 +0800

    增加时间播报

--- youtube.py
+++ youtube.py
@@ -4,6 +4,7 @@ import re
 from pyppeteer import launch
 from pyppeteer_stealth import stealth
 import itchat
+import time
 
 rule = r'title":{"runs":\[{"text":"(.*?)"}]'
 
@@ -18,6 +19,8 @@ async def main():
     content= await page.content()
 
     i=1
+    print(f"---新闻播报 {time.strftime('%Y年%m月%d日%H时')}---")
+    itchat.send(f"---新闻播报 {time.strftime('%Y年%m月%d日%H时')}---",toUserName=to_name[0]['UserName'])
     for title in re.findall(rule, content):
         if(title == "今天"):
             continue
