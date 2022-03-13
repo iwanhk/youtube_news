948b2e90fc70314b12d50465ea1fedda1d364770
iwanhk <iwan@isotop.top>
2022-03-13 23:30:43 +0800

    更改了编码解析

--- youtube.py
+++ youtube.py
@@ -1,10 +1,12 @@
-import asyncio
 #coding:utf8
+import asyncio
 import re
 from pyppeteer import launch
 from pyppeteer_stealth import stealth
 import itchat
 import time
+
+import urllib.parse
 
 rule = r'title":{"runs":\[{"text":"(.*?)"}]'
 
@@ -12,23 +14,23 @@ async def main():
     itchat.auto_login(enableCmdQR=False,hotReload=True)
     to_name = itchat.search_friends(name="理文")
 
-    browser = await launch(headless=True, userDataDir="/Users/iwan/Library/Application Support/Google/Chrome/Default", args=['--disable-infobars'])
+    browser = await launch(headless=False, userDataDir="/Users/iwan/Library/Application Support/Google/Chrome/Default", args=['--disable-infobars'])
     page = await browser.newPage()
     await stealth(page)  # <-- Here
     await page.goto('https://www.youtube.com/feed/subscriptions')
-    content= await page.content()
+    content= urllib.parse.unquote(await page.content())
 
     i=1
-    print(f"-------新闻播报 {time.strftime('%Y年%m月%d日%H时')}-------")
-    itchat.send(f"新闻播报 {time.strftime('%Y年%m月%d日%H时')}",toUserName=to_name[0]['UserName'])
+    info= f"新闻播报 {time.strftime('%Y年%m月%d日%H时')}\n"
     for title in re.findall(rule, content):
         if(title == "今天"):
             continue
         if(title == "昨天"):
             break
-        print(f"【{i}】{title}")
-        itchat.send(f"【{i}】{title}",toUserName=to_name[0]['UserName'])
+        info+= f"{i}-{title}\n"
         i+=1
+    print(info)
+    itchat.send(info,toUserName=to_name[0]['UserName'])
     
     #await browser.close()
 
